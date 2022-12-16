9fbddbb478b995f3c49ba2f44acb6a3eeda69494
Matthias Tylkowski <matthias.tylkowski@b-tu.de>
2022-12-16 16:38:29 +0100

    (chore) update version and maturity

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_bycategory';
-$plugin->release = '0.1.0';
-$plugin->version = 2022121500;
+$plugin->release = '1.0.0';
+$plugin->version = 2022121600;
 $plugin->requires = 2019052000;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
