b12650f09b352f4a1660dd6b9d5b0cfbb9e38349
Matthias Tylkowski <matthias.tylkowski@b-tu.de>
2023-01-02 16:11:05 +0100

    [bug] fix select all, formatting

--- amd/src/select-all.js
+++ amd/src/select-all.js
@@ -1,30 +1,72 @@
 import Selectors from './local/select-all/selectors';
 
 const registerEventListeners = () => {
-  const masterElem = document.querySelector(Selectors.selectors.masterSelector);
-  const checkboxElems = Array.from(document.querySelectorAll(Selectors.selectors.checkboxesSelector));
+    const masterElem = document.querySelector(Selectors.selectors.masterSelector);
+    const checkboxElems = Array.from(document.querySelectorAll(Selectors.selectors.checkboxesSelector));
 
-  if(!masterElem) {
-    throw new Error('select all element doesn\'t exist');
-  }
-
-  masterElem.addEventListener('change', e => {
-    const value = e.target.checked;
-    checkboxElems.forEach((elem) => {
-      elem.checked = value;
-    });
-  });
-
-  document.addEventListener('change', e => {
-    if(e.target === masterElem || checkboxElems.includes(e.target) === false) {
-      return;
+    if (!masterElem) {
+        throw new Error('select all element doesn\'t exist');
     }
 
-    masterElem.indeterminate = true;
-    masterElem.checked = false;
-  });
+    masterElem.addEventListener('change', e => {
+        const state = e.target.checked;
+        if (state === true) {
+            checkAll();
+        } else {
+            uncheckAll();
+        }
+    });
+
+    document.addEventListener('change', e => {
+        // Ignore masterElem or other Elements which are not in checkBoxElems Array.
+        if (e.target === masterElem || checkboxElems.includes(e.target) === false) {
+            return;
+        }
+
+        const allChecked = checkboxElems.every((elem) => elem.checked === true);
+        const allUnchecked = checkboxElems.every((elem) => elem.checked === false);
+
+        if (allChecked === true) {
+            masterToChecked();
+            return;
+        }
+
+        if (allUnchecked === true) {
+            masterToUnchecked();
+            return;
+        }
+
+        masterToIndeterminate();
+    });
+
+    const checkAll = () => {
+        checkboxElems.forEach((elem) => {
+            elem.checked = true;
+        });
+    };
+
+    const uncheckAll = () => {
+        checkboxElems.forEach((elem) => {
+            elem.checked = false;
+        });
+    };
+
+    const masterToIndeterminate = () => {
+        masterElem.indeterminate = true;
+        masterElem.checked = false;
+    };
+
+    const masterToChecked = () => {
+        masterElem.indeterminate = false;
+        masterElem.checked = true;
+    };
+
+    const masterToUnchecked = () => {
+        masterElem.indeterminate = false;
+        masterElem.checked = false;
+    };
 };
 
 export const init = () => {
-  registerEventListeners();
+    registerEventListeners();
 };
