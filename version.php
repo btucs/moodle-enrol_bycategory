9291397f3960f84c6aa9eb938d5b734769f0bc27
Matthias Tylkowski <matthias.tylkowski@b-tu.de>
2023-04-06 10:47:22 +0200

    release 1.1.3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_bycategory';
-$plugin->release = '1.1.2';
-$plugin->version = 2023010602;
+$plugin->release = '1.1.3';
+$plugin->version = 2023040600;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
