3a4c1cd04f797bf5bb8c3b4a06c4a2adc8868485
Matthias Tylkowski <matthias.tylkowski@b-tu.de>
2023-04-06 11:48:24 +0200

    task: fix linting issues

--- amd/src/repository.js
+++ amd/src/repository.js
@@ -1,4 +1,4 @@
-import { call as fetchMany } from 'core/ajax';
+import {call as fetchMany} from 'core/ajax';
 
 export const getEnrolmentMethods = (courseid) => fetchMany([{
   methodname: 'enrol_bycategory_get_enrolment_methods',
