d0aaf98ba31b29faaa9b53492bd3ac21035b6b94
Matthias Tylkowski <matthias.tylkowski@b-tu.de>
2023-09-22 15:15:40 +0200

    task: ignore phpcs rule moodle.PHPUnit.TestCaseNames.Missing

--- tests/util.php
+++ tests/util.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:disable moodle.PHPUnit.TestCaseNames.Missing
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
